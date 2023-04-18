5cca2a9c9de38e265ca1d23376efe4eea8e8799d
sewesleroux <sewes@outdoorphoto.co.za>
2023-04-18 16:16:07 +0200

    render the note and note page

--- demo/index.php
+++ demo/index.php
@@ -1,15 +1,5 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
 require 'Database.php';
-
-$config = require('config.php');
-$db = new Database($config['database']);
-
-$id = $_GET['id'];
-$query = "select * from posts where id = :id";
-
-$posts = $db->query($query, [':id' => $id])->fetch();
-
-dd($posts);
+require 'router.php';
