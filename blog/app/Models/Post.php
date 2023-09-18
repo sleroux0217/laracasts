6e4ca802e208c857f0311dfa023650e651ec2466
sewesleroux <sewes@outdoorphoto.co.za>
2023-09-18 10:11:57 +0200

    laravel blog incoming

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -4,25 +4,47 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
 
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
     public static function all()
     {
-        $files = File::files(resource_path("/posts/"));
-
-       return array_map(function ($file) {
-        return $file->getContents();
-        }, $files);
-
+        return cache()->rememberForever('posts.all', function () {
+            return collect(File::files(resource_path("posts")))
+                ->map(function ($file) {
+                    return YamlFrontMatter::parseFile($file);
+                })
+                ->map(function ($document) {
+                    return new Post(
+                        $document->title,
+                        $document->excerpt,
+                        $document->date,
+                        $document->body(),
+                        $document->slug,
+                    );
+                })
+                ->sortByDesc('date');
+        });
     }
     public static function find($slug)
     {
-        if (!file_exists($path = resource_path("/posts/{$slug}.html"))) {
-        throw new ModelNotFoundException();
-    }
-
-    return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+        return static::all()->firstWhere('slug', $slug);
     }
 }
--- blog/resources/views/post.blade.php
+++ blog/resources/views/post.blade.php
@@ -4,7 +4,11 @@
 
 <body>
 <article>
-   <?= $post ?>
+   <h1><?= $post->title; ?></h1>
+
+    <div>
+        <?= $post->body; ?>
+    </div>
 </article>
 
 <a href="/">Go Back</a>
